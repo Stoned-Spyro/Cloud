269610b26bc0345a7c67f1e9133771fbf0e7a455
Slavych Oleksii <slavych.oleksii@techno4.online>
2023-05-10 08:47:35 +0300

    Task 1 changed

--- Task1/provision.sh
+++ Task1/provision.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-aws s3api create-bucket --bucket slavychlab1 --region us-east-1                              
+BUCKET="slavychlab1"
 
-aws s3api put-bucket-policy --bucket slavychlab1 --policy file://policy.json         
+aws s3api create-bucket --bucket $BUCKET --region us-east-1     
 
-aws s3 sync ./ s3://slavychlab1/                                                             
+aws s3api delete-public-access-block --bucket $BUCKET
 
-aws s3 website s3://slavychlab1/ --index-document index.html --error-document error.html
+aws s3api put-bucket-policy --bucket $BUCKET --policy file://policy.json         
+
+aws s3 sync ./ s3://$BUCKET/                                                             
+
+aws s3 website s3://$BUCKET/ --index-document index.html --error-document error.html
 
 #website url: http://slavychlab1.s3-website-us-east-1.amazonaws.com/
